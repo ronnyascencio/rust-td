a220e43be819d9bfec33fa52e053f1a34c853ff1
Ronny Ascencio <ronnycompartist@gmail.com>
2026-01-05 06:27:00 -0500

    feat:  list

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod dos;
+mod tres;
 mod uno;
 
 fn main() {
     uno::say_hello();
     let _ = dos::create_folders("rust_folder");
-    println!("{}", uno::shot_name("test", "lighting", 3))
+    let list_items = vec!["test", "01", "03"];
+    let _list: () = tres::read_list(&list_items);
+    println!("{}", uno::shot_name("test", "lighting", 3));
 }
+
+
+
---
+++ src/tres.rs
@@ -0,0 +1,7 @@
+pub fn read_list(items: &Vec<&str>) {
+    
+    for i in items {
+        println!("{:?}:{:?}", "List item", i);
+    }
+}
+
